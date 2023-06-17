5cbf1b254365ce657eedc1be1b5098929ff40827
Jimmy R <jimmyroland@pm.me>
2023-06-17 08:21:50 -0500

    init

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-12 absolute top-0 right-0 w-full h-full flex flex-col justify-center uix-mission mb-500:p-4">
+      <video id="my-video"
+    class="video-js"
+    controls
+    preload="auto"
+    width="640"
+    height="264"
+    data-setup="{}">
+        <source src="https://custbuck-a-dev.nyc3.cdn.digitaloceanspaces.com/big_buck_bunny_720p_30mb.mp4" type="video/mp4" />
+      </video>
+  </div>
+</template>
